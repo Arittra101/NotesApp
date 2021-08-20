067ea7777e8d75074d115f289f6f06fe8adbad74
Avishek202 <avishek51775@gmail.com>
2021-08-20 11:36:50 +0600

    splash updated

--- Aust_Project/app/src/main/java/com/example/project/splashscreen.java
+++ Aust_Project/app/src/main/java/com/example/project/splashscreen.java
@@ -16,13 +16,12 @@ public class splashscreen extends AppCompatActivity {
         setContentView(R.layout.activity_splashscreen);
 
         getSupportActionBar().hide();
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     sleep(2000);
-
 
                 } catch (InterruptedException e) {
                     e.printStackTrace();
